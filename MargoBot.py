0e3e36d76e274dbb145e257f6d9c3f2ad02ff75a
kakliniew <238896@student.pwr.edu.pl>
2021-05-27 23:11:28 +0200

    loop corrected, new checks, captcha works better

--- MargoBot.py
+++ MargoBot.py
@@ -19,11 +19,12 @@ class MargoBot:
             self.check_if_captcha_and_resolve()
             self.skip_appearing_window_and_get_to_the_battle()
         while self.running:
-            if not self.find_element_and_click('images/autobutton.png', 0.5):
+            if not self.find_element_and_click('images/autobutton.png', 0.4):
                 if not self.check_if_captcha_and_resolve():
                     if not self.check_if_stamina_gone_and_end():
                         break
                 self.find_element_and_click('images/autobutton.png', self.timeout)
+            self.wait_for_end_of_batte_to_be_visible_for_time(0.3)
             self.check_if_low_hp_and_heal()
             self.find_element_and_click('images/nextbattle.png', self.timeout)
 
@@ -40,17 +41,17 @@ class MargoBot:
         found_elements = self.cv_act.find_elements_from_image_with_wait(path, self.default_screenshot_path, timeout)
         self.sel_act.click_element(found_elements[0])
 
-    # def wait_for_element_to_be_visible_for_time(self, path, timeout):
-    #     found_elements = self.cv_act.find_elements_from_image_with_wait(path, 'screenshots/actualView.png', timeout)
-    #     count_founds = len(found_elements)
-    #     if count_founds > 0:
-    #         print("Element from " + path + " was found")
-    #     else:
-    #         print("Element from " + path + " couldn't be find, trying to repair...")
-    #         self.repair_function()
+    def wait_for_end_of_batte_to_be_visible_for_time(self, timeout):
+        found_elements = self.cv_act.find_elements_from_image_with_wait('images/zwyciestwo.png',
+                                                                        'screenshots/actualView.png', timeout)
+        count_founds = len(found_elements)
+        if count_founds > 0:
+            print("Element from " + "'images/zwyciestwo.png'" + " was found")
+        else:
+            print("Element from " + "'images/zwyciestwo.png'" + " couldn't be find, looking for 'przegrana'")
+            self.cv_act.find_elements_from_image_with_wait('images/przegrana.png', 'screenshots/actualView.png',
+                                                           timeout)
 
-    # def repair_function(self):
-    #     pass
 
     def send_keys_and_confirm(self, text):
         self.sel_act.send_keys(text)
@@ -69,27 +70,31 @@ class MargoBot:
         if self.cv_act.is_element_visible_after_seconds('images/arcymagnaszyjniki.png', self.default_screenshot_path,
                                                         0.2):
             to_search = 'naszyjnik'
+        elif self.cv_act.is_element_visible_after_seconds('images/arcymagmiecze.png', self.default_screenshot_path,
+                                                          0.2):
+            to_search = 'miecz'
+        elif self.cv_act.is_element_visible_after_seconds('images/arcymagrekawice.png', self.default_screenshot_path,
+                                                          0.2):
+            to_search = 'rekawice'
+        elif self.cv_act.is_element_visible_after_seconds('images/arcymagkaptury.png', self.default_screenshot_path,
+                                                          0.2):
+            to_search = 'helm'
         elif self.cv_act.is_element_visible_after_seconds('images/arcymagbuty.png', self.default_screenshot_path, 0.2):
             to_search = 'buty'
         elif self.cv_act.is_element_visible_after_seconds('images/arcymagpierscienie.png', self.default_screenshot_path,
                                                           0.2):
             to_search = 'pierscien'
-        elif self.cv_act.is_element_visible_after_seconds('images/arcymaghelmy.png', self.default_screenshot_path, 0.2):
-            to_search = 'helm'
-        elif self.cv_act.is_element_visible_after_seconds('images/arcymagrekawice.png', self.default_screenshot_path,
-                                                          0.2):
-            to_search = 'rekawice'
         elif self.cv_act.is_element_visible_after_seconds('images/arcymagplaszcz.png', self.default_screenshot_path,
                                                           0.2):
             to_search = 'plaszcz'
-        elif self.cv_act.is_element_visible_after_seconds('images/arcymagmiecze.png', self.default_screenshot_path,
-                                                          0.2):
-            to_search = 'miecz'
 
+        print("to search " + to_search)
         found_elements = self.cv_act.find_elements_from_image_with_wait('images/' + to_search + '.png',
                                                                         self.default_screenshot_path, self.timeout)
         empty_places = self.cv_act.find_elements_from_image_with_wait('images/slotarcymag.png',
                                                                       self.default_screenshot_path, self.timeout)
+        print("znalezione elementy" + found_elements)
+        print("znalezione puste miesjsca " + empty_places)
         for i in range(empty_places):
             self.sel_act.hold_and_moveto(found_elements[i], empty_places[i])
 
@@ -121,17 +126,21 @@ class MargoBot:
                 return False
 
     def check_if_low_hp_and_heal(self):
-        sleep(1)
         bgr = self.cv_act.get_color_of_pixel(self.default_screenshot_path, 215, 445)
-        while not (bgr[0] == 17 and bgr[1] == 17 and bgr[2] == 167):
+        print(bgr)
+        while bgr[0] != 17 and bgr[1] != 17 and bgr[2] != 167 and bgr[0] != 10 and bgr[1] != 10 and bgr[2] != 102:
             self.heal_the_character()
+            sleep(1)
             bgr = self.cv_act.get_color_of_pixel(self.default_screenshot_path, 215, 445)
+            print(bgr)
 
     def check_if_stamina_gone_and_end(self):
         if self.cv_act.is_element_visible_after_seconds('images/brakstaminy2.png', self.default_screenshot_path, 0.3):
             self.running = False
+            print("zakonczono z powodu braku staminy")
         elif self.cv_act.is_element_visible_after_seconds('images/brakstaminy.png', self.default_screenshot_path, 0.2):
             self.running = False
+            print("zakonczono z powodu braku staminy - komunikat drugi")
         else:
             self.running = False
             print("Dziwny stan aplikacji, nie rozpoznano")
--- OpencvActions.py
+++ OpencvActions.py
@@ -11,7 +11,7 @@ class OpencvActions:
     def find_matches(self, screenshot, template, threshold):
         w, h = template.shape[::-1]
 
-        res = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)  # @UndefinedVariable
+        res = cv2.matchTemplate(screenshot, template, cv2.TM_CCOEFF_NORMED)
         loc = np.where(res >= threshold)
         count = 0
         points = []
@@ -24,7 +24,7 @@ class OpencvActions:
         return points
 
     def find_elements_from_image_with_wait(self, loading_img_path, loading_scrn_path, timeout=30):
-        load_img = cv2.imread(loading_img_path, 0)  # Loading Image @UndefinedVariable
+        load_img = cv2.imread(loading_img_path, 0)  # Loading Image
         time_spent = 0
         count_founds = 0
         found_points = None
@@ -34,7 +34,7 @@ class OpencvActions:
             self.driver.save_screenshot(loading_scrn_path)
             print("Screenshot saved")
             print("Looking for Element " + loading_img_path)
-            load_scrn = cv2.imread(loading_scrn_path, 0)  # Loading Screenshot @UndefinedVariable
+            load_scrn = cv2.imread(loading_scrn_path, 0)  # Loading Screenshot
             found_points = self.find_matches(load_scrn, load_img, 0.99)
             count_founds = len(found_points)
             sleep(delay_time)
@@ -53,45 +53,3 @@ class OpencvActions:
     def is_element_visible_after_seconds(self, loading_img_path, loading_scrn_path, timeout=30):
         return self.find_elements_from_image_with_wait(loading_img_path, loading_scrn_path, timeout)
 
-    # def skip_ads(self, close_img_path, close_img_alt_path, ad_scrn_path, timeout):
-    #     close_img = cv2.imread(close_img_path, 0)  # Close ad Image @UndefinedVariable
-    #     close_img_alt = cv2.imread(close_img_alt_path, 0)  # Close ad alternative Image @UndefinedVariable
-    #
-    #     print(self.driver.save_screenshot(ad_scrn_path))
-    #
-    #     ad_scrn = cv2.imread(ad_scrn_path, 0)  # Ad Screenshot @UndefinedVariable
-    #
-    #     # Default timeout is 60 seconds
-    #     if (timeout == NULL):
-    #         timeout = 60
-    #
-    #     time_spent = 0
-    #     count = 0
-    #
-    #     while ((count < 1) & (time_spent < timeout)):
-    #         # Save a screenshot of the screen
-    #         self.driver.save_screenshot(ad_scrn_path)
-    #
-    #         ad_scrn = cv2.imread(ad_scrn_path, 0)  # Ad Screenshot @UndefinedVariable
-    #         close_ad = self.find_matches(ad_scrn, close_img, 0.9)
-    #         count = len(close_ad)
-    #         # First close image not found, try to find the alternative one
-    #         if (count == 0):
-    #             close_ad = self.find_matches(ad_scrn, close_img_alt, 0.9)
-    #             count = len(close_ad)
-    #         sleep(2)
-    #         time_spent += 2
-    #
-    #     if (time_spent >= timeout):
-    #         print("Test failed! Ad time exceeded timeout of ", timeout, " seconds!")
-    #         self.driver.quit()
-    #     else:
-    #         if (count > 1):
-    #             close_point = []
-    #             close_point.append(close_ad[0])
-    #         else:
-    #             close_point = close_ad
-    #         self.driver.tap(close_point)
-    #         print("Ad closed in aprox. ", time_spent, " seconds.")
-    #         sleep(1)
-    #
